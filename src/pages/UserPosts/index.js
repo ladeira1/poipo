ce65b6fb97c54268d64303c3761ab9bbf09732be
Victor Ladeira <victorladeirag@gmail.com>
2020-10-08 18:25:17 -0300

    updated userPosts screen

--- src/pages/UserPosts/index.js
+++ src/pages/UserPosts/index.js
@@ -2,12 +2,15 @@ import React, {useState, useLayoutEffect, useEffect, useContext} from 'react'
 import {
   View,
   ActivityIndicator,
+  TouchableOpacity,
+  Text
 } from 'react-native'
 import {
   Container,
   List,
 } from './styles'
 import colors from '../../../styles/colors'
+import Icon from 'react-native-vector-icons/Feather'
 
 import Post from '../../components/Post'
 
@@ -23,15 +26,23 @@ export default UserPosts = ({route}) => {
   const [title, setTitle] = useState(route.params.title)
   const [posts, setPosts] = useState([])
 
+  const handleFriend = () => {
+    alert('to do')
+  }
+
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: title
+      title: title,
+      headerRight: () => (
+        <TouchableOpacity onPress = {handleFriend} style = {{marginRight: 10}} >
+          <Icon name = 'user-plus' size = {25} color = {colors.primaryPurple} />
+        </TouchableOpacity>
+      )
     })
   }, [title, navigation])
 
   useEffect(() => {
     const user = loadUserPosts(route.params.userId, setPosts, setLoading)
-
     return () => (
       user
     )
@@ -48,12 +59,12 @@ export default UserPosts = ({route}) => {
             <ActivityIndicator size = {50} color = {colors.primaryPurple} />
           </View>
         ) : (
-          <List 
-          showsVerticalScrollIndicator = {false}
-          data = {posts}
-          renderItem = {({item}) => (
-            <Post data = {item} userId = {user.uid} />
-          )} />
+            <List 
+            showsVerticalScrollIndicator = {false}
+            data = {posts}
+            renderItem = {({item}) => (
+              <Post data = {item} userId = {user.uid} />
+            )} />
         )
       }
     </Container>
